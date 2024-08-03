e3b822cb8df3cf55aace00c73cf444021282adec
diegun99 <diegun123456789@gmail.com>
2024-08-02 21:06:34 -0500

    definiendo appdbcontext para consolidar los modelos de las tablas de la base de datos

--- CrudNuevaVersionAPI/Context/AppDbContext.cs
+++ CrudNuevaVersionAPI/Context/AppDbContext.cs
@@ -3,8 +3,26 @@ using CrudNuevaVersionAPI.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace CrudNuevaVersionAPI.Context
-{
-    public class AppDbContext
+{/// se crea la clase la cual creara todos los méotods que se conectaran a la base de datos
+    // para que sea usada como un contexto de bd
+    public class AppDbContext : DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+
+        // añadir entidades para que sean seteadas como unas tablas
+
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Perfil> Perfiles { get; set; }
+
+        // modelado de la informaxion  // definir como esraran estructuradas las tablas
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
     }
 }
